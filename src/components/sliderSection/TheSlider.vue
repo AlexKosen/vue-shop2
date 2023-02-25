74c035e30b1f77006ee9ef7b632a0357dc30876f
AlexKosen <121030488+AlexKosen@users.noreply.github.com>
2023-02-25 22:31:46 +0200

    Add

--- src/components/sliderSection/TheSlider.vue
+++ src/components/sliderSection/TheSlider.vue
@@ -1,41 +1,20 @@
 <script>
 import TheSliderItem from "./TheSliderItem.vue";
-import TheCarouselIndicators from "./TheCarouselIndicators.vue"
 import { defineComponent } from "vue";
 import { Carousel, Pagination, Slide } from "vue3-carousel";
-import { ref } from 'vue'
 import "vue3-carousel/dist/carousel.css";
-
 
 export default defineComponent({
   name: "Autoplay",
   components: {
     TheSliderItem,
-    TheCarouselIndicators,
     Carousel,
     Slide,
     Pagination,
   },
- 
-  data() {
-    return {
-      myCarousel: ref(null),
-      isActive: false,
-      currentSlide: 0
-    }
-  },
-  
-  methods: {
-    slideTo(numb) {
-      console.log(this.currentSlide)
-      // this.isActive = !this.isActive
-      console.log(this.myCarousel.next())
-    }
-    
-  },
 
-  mounted() {
-    
+  data() {
+    return {};
   },
 });
 </script>
@@ -45,38 +24,60 @@ export default defineComponent({
     <div class="carousel-inner">
       <div class="carousel-item active">
         <div id="customCarousel1" class="carousel slide" data-ride="carousel">
-          <Carousel
-            ref="myCarousel"
-            v-model="currentSlide" 
-            :autoplay="2000" 
-            :wrap-around="true" 
-            :transition="1500">
-
-            <Slide v-for="slide in 3" 
-            :key="slide"
+          <Carousel :autoplay="2000" :wrap-around="true" :transition="1500">
+            <Slide
+              v-for="(slide, index) in 3"
+              :key="slide"
+              :currentSlide="index"
             >
               <TheSliderItem />
             </Slide>
 
             <template #addons>
-              <ol class="carousel-indicators">
-                <TheCarouselIndicators
-                v-model="currentSlide"
-                v-for="indicators in 3"
-                :key="indicators" 
-                :class="{ active: isActive }"
-                @click="slideTo"           
-                />
-                </ol>
+              <Pagination />
             </template>
           </Carousel>
-
-
-          
         </div>
       </div>
     </div>
   </section>
 </template>
+<style lang="scss">
+.carousel__pagination {
+  position: unset;
+  margin: 0px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 45px;
+}
 
-
+.carousel__pagination-button {
+  position: unset;
+  justify-content: center;
+  align-items: center;
+  margin-top: 45px;
+  border-radius: 100%;
+  outline: 0 !important;
+  ::-moz-focus-outer,
+  ::-moz-focus-inner {
+    border: 0;
+    padding: 0;
+  }
+}
+.carousel__pagination-button::after {
+  background-color: rgb(255, 255, 255);
+  width: 15px;
+  height: 15px;
+  border-radius: 100%;
+  opacity: 1;
+}
+.carousel__pagination-button:hover::after,
+.carousel__pagination-button--active::after {
+  width: 23px;
+  height: 23px;
+  background-color: #f0d43a;
+  position: unset;
+  justify-content: center;
+  align-items: center;
+}
+</style>
